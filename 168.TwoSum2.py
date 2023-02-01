8ed016bf7f358c73c3879087c0da74752b308971
Parmeet Chawla <ps3chawl@uwaterloo.ca>
2023-02-01 17:10:47 +0530

    Two Sum II redifined

--- 168.TwoSum2.py
+++ 168.TwoSum2.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         indexFront = 0
         indexEnd = len(numbers) - 1
-        while True:
+        while indexFront < indexEnd:
             if numbers[indexFront] + numbers[indexEnd] == target:
                 return [indexFront + 1, indexEnd + 1]
             elif numbers[indexFront] + numbers[indexEnd] > target:
